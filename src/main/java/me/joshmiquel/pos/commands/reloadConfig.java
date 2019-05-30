25f87fcf7c3d13883bd763a606c780d60081c6b6
JoShMiQueL <unconfigured@null.spigotmc.org>
2019-05-30 21:49:38 +0200

    CustomConfiguration deleted

--- src/main/java/me/joshmiquel/pos/commands/reloadConfig.java
+++ src/main/java/me/joshmiquel/pos/commands/reloadConfig.java
@@ -1,17 +1,9 @@
 package me.joshmiquel.pos.commands;
 
 import me.joshmiquel.pos.Main;
-import me.joshmiquel.pos.util.CustomConfiguration;
-import me.joshmiquel.pos.util.FileBuilder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-
-import java.io.File;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class reloadConfig implements CommandExecutor {
 
